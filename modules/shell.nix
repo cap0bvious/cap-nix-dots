a79350204ad2ab9263644b266bb3818deaf8e1a5
cap0bvious <kap0bvious22@gmail.com>
2026-09-12 13:38:13 +0300

    fiix the shame

--- modules/shell.nix
+++ modules/shell.nix
@@ -1,20 +1,20 @@
 { pkgs, ... }:
 
 {
-  programs.fish = {
-    enable = true;
-    loginShellInit = ''
-      if status is-login
-        if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
-          exec start-hyprland
+    programs.fish = {
+        enable = true;
+        loginShellInit = ''
+        if status is-login
+            if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
+            exec start-hyprland
+            end
         end
-      end
-    '';
-    interactiveShellInit = ''
-      set fish_greeting # Disable greeting
-    '';
-  };
-  users.extraUsers."capobvious" = {
-    shell = pkgs.fish;
-  };
+        '';
+        interactiveShellInit = ''
+        set fish_greeting # Disable greeting
+        '';
+    };
+    users.extraUsers."capobvious" = {
+        shell = pkgs.fish;
+    };
 }
